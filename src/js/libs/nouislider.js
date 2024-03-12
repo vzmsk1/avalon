b1e788540b30f36ce2bfb753bf7271abed4837c7
ukik0 <ctaricc@mail.ru>
2024-03-12 22:29:49 +0300

    feat: catalog page desktop

---
+++ src/js/libs/nouislider.js
@@ -0,0 +1,42 @@
+import noUiSlider from 'nouislider';
+
+export const rangeSlider = document.querySelector('.range-slider');
+document.addEventListener('DOMContentLoaded', () => {
+    if (rangeSlider) {
+        noUiSlider.create(rangeSlider, {
+            range: {
+                min: 20000,
+                max: 850000
+            },
+            step: 1,
+            start: [20000, 550000],
+            connect: true,
+            format: {
+                to: function (value) {
+                    return Math.round(value).toLocaleString('ru-RU') + ' ₽';
+                },
+                from: function (value) {
+                    return value.replace(' ₽', '');
+                }
+            }
+        });
+
+        const inputFrom = document.getElementById('from');
+        const inputTo = document.getElementById('to');
+
+        const inputs = [inputFrom, inputTo];
+
+        rangeSlider.noUiSlider.on('update', (values, handle) => {
+            inputs[handle].value = values[handle];
+        });
+
+        inputs.forEach((input, index) => {
+            input.addEventListener('change', (event) => {
+                const range = [null, null];
+                range[index] = event.currentTarget.value;
+
+                rangeSlider.noUiSlider.set(range);
+            });
+        });
+    }
+});
