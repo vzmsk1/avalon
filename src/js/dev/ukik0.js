79567fba012dc5923c30f1ae1190aa13c4b38604
ukik0 <ctaricc@mail.ru>
2024-03-18 23:42:44 +0300

    feat: catalog 2-nd and 3-rd level pages mobile versions

--- src/js/dev/ukik0.js
+++ src/js/dev/ukik0.js
@@ -1,3 +1,5 @@
+import { bodyLock, bodyLockStatus, bodyUnlock } from '../utils/utils';
+
 document.addEventListener('DOMContentLoaded', () => {
     if (document.querySelector('.communication')) {
         (async () => {
@@ -28,5 +30,25 @@ document.addEventListener('DOMContentLoaded', () => {
             map.addChild(new ymaps3.YMapDefaultFeaturesLayer({ zIndex: 1800 }));
             map.addChild(marker);
         })();
+    }
+
+    if (document.querySelector('.catalog__filters')) {
+        const filtersOpenButton = document.querySelector('.catalog__selection-button');
+        const filtersCloseButton = document.querySelector('.catalog__filters-heading-button');
+        const filtersMenu = document.querySelector('.catalog__filters');
+
+        filtersOpenButton.addEventListener('click', () => {
+            filtersMenu.classList.add('--active');
+
+            bodyLock();
+        });
+
+        filtersCloseButton.addEventListener('click', (event) => {
+            event.preventDefault();
+
+            filtersMenu.classList.remove('--active');
+
+            bodyUnlock();
+        });
     }
 });
