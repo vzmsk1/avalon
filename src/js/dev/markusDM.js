682fcc80889e74de25ab58f5063df97c03d958b5
Dmitry <mazarini200@yandex.ru>
2024-04-11 06:45:10 +0500

    update

--- src/js/dev/markusDM.js
+++ src/js/dev/markusDM.js
@@ -42,3 +42,11 @@ $( document ).ready(function() {
         });
     });
 });
+
+
+
+$(".catalog__filters-slide-fields").keypress(function(event){
+    event = event || window.event;
+    if (event.charCode && event.charCode!=0 && event.charCode!=46 && (event.charCode < 48 || event.charCode > 57) )
+      return false;
+});
